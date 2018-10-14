5feb42b47ceb66d31845d33d258ae26c6937d4a4
Diane Hu <dh224@duke.edu>
2018-10-14 13:03:10 -0400

    Syntax

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require(‘express’);
-var port = process.env.PORT || 3000;
-var app = express();
-app.get(‘/’, function (req, res) {
-res.send(JSON.stringify({ Hello: ‘World’}));
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send({ express: 'Hello From Express' });
 });
-app.listen(port, function () {
-    console.log(`Example app listening on port !`);
-});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
